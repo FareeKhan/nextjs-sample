69396e341a8c46548249c40abd72c33db515f074
Fareekhan <fareed989989@gmail.com>
2025-03-27 15:07:44 +0400

    www

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+// next.config.mjs
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/.well-known/apple-app-site-association",
+          headers: [
+            {
+              key: "Content-Type",
+              value: "application/json",
+            },
+            {
+              key: "Content-Disposition",
+              value: "inline",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
+  
