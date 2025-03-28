eb6675527c31e714ea5dc3a6d08faa4c2a308681
Fareekhan <fareed989989@gmail.com>
2025-03-28 11:48:11 +0400

    deeplinking

--- app/page.js
+++ app/page.js
@@ -3,12 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-      hello this is my firest website 
-      hello this is my firest website 
-      hello this is my firest website 
-      hello this is my firest website 
-      this os tesing
-      show me tangiwal
+  The best Part of your life is to do ALLAH ziker 
     </div>
   )
 }
